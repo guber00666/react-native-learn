2fc0875be29482d4035d7521d9dd6310482d6412
Zakharov Sergey <sergey.v.zakharov@rtlabs.ru>
2022-09-12 11:54:32 +0300

    Revert "react native todo list"
    This reverts commit 215a53c310cad43779288536b4b28c5152faf486.

--- App.tsx
+++ App.tsx
@@ -1,45 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navbar from "./src/Navbar";
-import AddTodo from "./src/AddTodo";
-import Todo from "./src/Todo/Todo";
-import {useState} from "react";
-
-type Todo = {
-    id: string,
-    title: string
-}
 
 export default function App() {
-    const [list, setList] = useState<Todo[]>([]);
-
-    return (
-        <View>
-        <Navbar title={"Todo App"} />
-            <View style={styles.container}>
-             <AddTodo onAdd={handleAdd} />
-                <View>
-                    {list.map((l, i) => (<Todo onDelete={handleDelete} idx={i} key={l.id} title={l.title}/>))}
-                </View>
-            </View>
-        </View>
-    );
-
-    function handleDelete(i: number){
-        list.splice(i, 1)
-        setList(prevState => [...list])
-    }
-
-    function handleAdd(title: string) {
-        setList((prevState) => [...prevState, {
-            id: Date.now().toString(),
-            title
-        }])
-    }
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 30,
-      paddingVertical: 20
-  }
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
